1d4fbf60a2b9f5bed62d0ded11a8ec1f2e7b4e06
MYAUNTIESPASSIVEAGGRESSIVE <adamkw123lightning@gmail.com>
2024-10-01 18:03:46 +0100

    first commit

---
+++ Assets/Scripts/PlayerScripts/PlayerControl.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    
+    [Header("Game Object References")]
+    public LayerMask GroundLayer;
+    private Rigidbody2D PlrRB;
+    public Vector2 CheckSize;
+
+    [Header("Movement Variables")]
+    public float MoveSpeed = 20f;
+    public float JumpHeight = 5f;
+    private float HozPos;
+    public float CastDistance = 1f;
+
+    void Start()
+    {
+        PlrRB = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        HozPos = Input.GetAxisRaw("Horizontal");
+
+        CharacterMove();   
+    }
+
+    private void CharacterMove()
+    {
+        PlrRB.velocity = new Vector2(HozPos * MoveSpeed, PlrRB.velocity.y);
+
+        // When jump button pressed + is grounded the player is able to jump
+        if (Input.GetButton("Jump") && GroundChecker())
+        {
+            PlrRB.velocity = new Vector2(PlrRB.velocity.x, JumpHeight);
+        }
+
+
+    }
+
+    // checks player is on ground/platform
+    public bool GroundChecker()
+    {
+        // if player is on ground layer then player can jump/ bool = true
+         if (Physics2D.BoxCast(transform.position, CheckSize, 0, -transform.up, CastDistance, GroundLayer))
+         {
+             return true;
+         }
+         else return false;
+    }
+
+    // draws debug box for groundcheck
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireCube(transform.position - transform.up * CastDistance, CheckSize);
+        Gizmos.color = Color.green;
+    }
+}
