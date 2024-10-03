43525c14cd4ab8e9d1050ca6f20def5297b715e9
MYAUNTIESPASSIVEAGGRESSIVE <adamkw123lightning@gmail.com>
2024-10-03 15:50:17 +0100

    Platforms + Boomerang Stuff

--- Assets/Scripts/PlayerScripts/BoomerangScript.cs
+++ Assets/Scripts/PlayerScripts/BoomerangScript.cs
@@ -1,18 +1,33 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class BoomerangScript : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [Header("References")]
+    public GameObject SpinPos;
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        BoomFollow();
+
+        if (Input.GetMouseButtonDown(0))
+        {
+
+        }
+    }
+
+    private void BoomFollow()
+    {
+        Vector3 Mpos = Input.mousePosition;
+        Mpos.z = 0;
+
+        Vector3 Wpos = Camera.main.ScreenToWorldPoint(Mpos);
     }
 }
