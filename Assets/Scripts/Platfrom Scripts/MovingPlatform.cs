3c7fe236845208751e8bb0963203d08bc931aea4
MYAUNTIESPASSIVEAGGRESSIVE <adamkw123lightning@gmail.com>
2024-10-03 23:14:46 +0100

    platform stuff

--- Assets/Scripts/Platfrom Scripts/MovingPlatform.cs
+++ Assets/Scripts/Platfrom Scripts/MovingPlatform.cs
@@ -1,18 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class MovingPlatform : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [Header("Start and End Positions")]
+    public Vector2 StartPos;
+    public Vector2 EndPos;
+
+    [Header("Platform Variables")]
+    public float MoveSpeed;
+    public float MoveDelay;
+
     void Start()
     {
-        
+        transform.position = StartPos;
     }
-
-    // Update is called once per frame
     void Update()
     {
-        
+        transform.position = Vector2.MoveTowards(transform.position, EndPos, MoveSpeed * Time.deltaTime);
+
+        if (transform.position == EndPos)
+        {
+
+        }
     }
 }
